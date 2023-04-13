23a2408f25fd6613d9c8f4a1706f5fae17a068c0
SAVAGE <proj_1>
2023-04-13 12:56:19 +0300

    all_done3

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Number();
+
+int Read()
+{
+    System.Console.WriteLine("введите число");
+    int N = int.Parse(Console.ReadLine());
+    return N;
+}
+
+void Number()
+{
+    int n = 1;
+    int N = Read();
+     while (n <= N)
+     {
+        double d = Math.Pow(n , 3);
+        System.Console.Write(d+" ");
+        n++;
+     }
+}
